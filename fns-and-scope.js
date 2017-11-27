39720adf5a1ce407e03a403c25dd3ffa0b12f5a6
Larry Li <larry.zy.li@gmail.com>
2017-11-27 16:28:11 -0600

    Changed Problem 5 Answer

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -12,11 +12,10 @@ var name = 'Tyler';
     }
   }
 
+
 //////////////////PROBLEM 2////////////////////
 
-
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-
 
   function getName() {
     var name = prompt();
@@ -25,8 +24,6 @@ var name = 'Tyler';
 
 
 //////////////////PROBLEM 3////////////////////
-
-
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
@@ -39,35 +36,36 @@ var name = 'Tyler';
 
 //////////////////PROBLEM 4////////////////////
 
-
-
-
 //What is the difference between arguments and parameters?
 
   //Arguments are what is passed into the function during the invocation
   //Parameters are what a function takes in during the function declaration
 
+
 //////////////////PROBLEM 5////////////////////
-
-
 
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
-
   //null, 0, undefined, '', false, NaN
 
+  //put the defined variable into an 'if' statement, for example:
+  // var checkThis = '';
+  // function checkFalsy (){
+  //   if (checkThis){
+  //     return 'Truthy.';
+  //   } else {
+  //     return 'Falsy.';
+  //   }
+  // }
+  // checkFalsy();
 
 
 //////////////////PROBLEM 6////////////////////
-
-
 
 //Create a function called myName that returns your name
 
   var myName = () => 'Larry';
   
-
-
 //Now save the function definition of myName into a new variable called newMyName
 
   var newMyName = myName;
@@ -77,10 +75,7 @@ var name = 'Tyler';
   alert(newMyName());
 
 
-
 //////////////////PROBLEM 7////////////////////
-
-
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
